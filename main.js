7c6d892f985f27e3269a3a4330efd98488a0c36e
habberzabib <145040111+habberzabib@users.noreply.github.com>
2023-09-20 09:58:41 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+document.getElementById("salt").addEventListener("click", firstClick);
+
+function firstClick() {
+  alert("salt.");
+}
+
+document.getElementById("hat").addEventListener("click", secondClick);
+
+function secondClick() {
+  alert("hat!");
+}
+
+document.getElementById("oneBasket").addEventListener("click", thirdClick);
+
+function thirdClick() {
+  alert("one basket.");
+}
+
+document.getElementById("medicine").addEventListener("click", fourthClick);
+
+function fourthClick() {
+  alert("medicine.");
+}
+
+document.getElementById("bliss").addEventListener("click", fifthClick);
+
+function fifthClick() {
+  alert("bliss.");
+}
